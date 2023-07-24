f2626959e2447e9e9cb31f252dc561f4a6803d2b
Théo Barreto Silva <barretotheo25@gmail.com>
2023-07-24 17:42:34 -0300

    a principio está pronto

--- weather.py
+++ weather.py
@@ -12,6 +12,42 @@ root.title("Weather App")
 root.geometry("900x500+300+200")
 root.resizable(False,False)
 
+def getWeather():
+    try:
+        city=textfield.get()
+        geolocator=Nominatim(user_agent="geoapiExercises")
+        location=geolocator.geocode(city)
+        obj=TimezoneFinder()
+        result=obj.timezone_at(lng=location.longitude, lat=location.latitude)
+        
+        home=pytz.timezone(result)
+        local_time=datetime.now(home)
+        current_time=local_time.strftime("%I:%M %p")
+        clock.config(text=current_time)
+        name.config(text="CURRENT WEATHER")
+        print(location.latitude)
+        print(location.longitude)
+
+        #weather
+        api="https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=4628860cab528f9a218d49d13efdbbd8"
+        json_data = requests.get(api).json()
+        condition = json_data['weather'][0]['main']
+        description=json_data['weather'][0]['description']
+        temp=int(json_data['main']['temp']-273.15)
+        pressure=json_data['main']['pressure']
+        humidity=json_data['main']['humidity']
+        wind=json_data['wind']['speed']
+
+        t.config(text=(temp,"°"))
+        c.config(text=(condition, "|", "FEELS", "LIKE", temp, "°"))
+        w.config(text=wind)
+        h.config(text=humidity)
+        d.config(text=description)
+        p.config(text=pressure)
+    except Exception as e:
+        messagebox.showerror("Weather App", "Invalid Entry!!")
+
+
 #search box
 Search_image=PhotoImage(file="search.png")
 myimage=Label(image=Search_image)
@@ -22,7 +58,7 @@ textfield.place(x=50, y=40)
 textfield.focus()
 
 Search_icon=PhotoImage(file="search_icon.png")
-myimage_icon=Button(image=Search_icon, borderwidth=0, cursor="hand2",bg="#404040")
+myimage_icon=Button(image=Search_icon, borderwidth=0, cursor="hand2",bg="#404040", command=getWeather)
 myimage_icon.place(x=400,y=34)
 
 #logo
@@ -34,6 +70,12 @@ logo.place(x=150,y=100)
 Frame_image=PhotoImage(file="box.png")
 frame_myimage=Label(image=Frame_image)
 frame_myimage.pack(padx=5, pady=5, side=BOTTOM)
+
+#time
+name=Label(root,font=("arial", 15, "bold"))
+name.place(x=30, y=100)
+clock=Label(root, font=("Helvetica", 20))
+clock.place(x=30, y=130)
 
 #label
 label1=Label(root, text="WIND", font=("Helvetica", 15, 'bold'), fg="white", bg="#1ab5ef")
@@ -57,12 +99,12 @@ w=Label(text="...", font=("arial", 20, "bold"), bg="#1ab5ef")
 w.place(x=120, y=430)
 
 h=Label(text="...", font=("arial", 20, "bold"), bg="#1ab5ef")
-h.place(x=250, y=430)
+h.place(x=280, y=430)
 
 d=Label(text="...", font=("arial", 20, "bold"), bg="#1ab5ef")
-d.place(x=430, y=430)
+d.place(x=450, y=430)
 
 p=Label(text="...", font=("arial", 20, "bold"), bg="#1ab5ef")
-p.place(x=120, y=430)
+p.place(x=670, y=430)
 
 root.mainloop()
